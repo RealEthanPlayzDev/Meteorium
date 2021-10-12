c552b53ed6b02c6981b8c235513d2cdf68de55f6
RealEthanPlayzDev <ethan.budianto@gmail.com>
2021-10-12 13:45:12 +0700

    Fix unban

--- src/commands/moderation/unban.js
+++ src/commands/moderation/unban.js
@@ -5,15 +5,27 @@ const MeteoriumCommand = require("../../util/Command");
 module.exports = new MeteoriumCommand("unban", "Unbans a user using their UserId", async (interaction, client) => {
     if (interaction.member.permissions.has("BAN_MEMBERS", true)) {
         const userId = interaction.options.getString("userid");
-        interaction.guild.members.unban(userId, `Administrative unban issued by ${interaction.user.username}`);
-        await interaction.reply({ embeds: [
-            new MessageEmbed()
-                .setTitle("User unbanned")
-                .setDescription("User has been successfully unbanned")
-                .setColor("0099ff")
-                .setFooter("Meteorium | Developed by RadiatedExodus (RealEthanPlayzDev)")
-                .setTimestamp()
-        ]});
+        const banList = await interaction.guild.bans.fetch();
+        if (banList.has(userId)) {
+            interaction.guild.members.unban(userId, `Administrative unban issued by ${interaction.user.username}`);
+            await interaction.reply({ embeds: [
+                new MessageEmbed()
+                    .setTitle("User unbanned")
+                    .setDescription("User has been successfully unbanned")
+                    .setColor("0099ff")
+                    .setFooter("Meteorium | Developed by RadiatedExodus (RealEthanPlayzDev)")
+                    .setTimestamp()
+            ]});
+        } else {
+            await interaction.reply({ embeds: [
+                new MessageEmbed()
+                    .setTitle("Cannot unban user")
+                    .setDescription("This user isn't banned!")
+                    .setColor("0099ff")
+                    .setFooter("Meteorium | Developed by RadiatedExodus (RealEthanPlayzDev)")
+                    .setTimestamp()
+            ]});
+        }
     } else {
         await interaction.reply({ embeds: [
             new MessageEmbed()
