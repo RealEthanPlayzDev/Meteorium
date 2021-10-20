004a382a68619f69fa2ea90f71c5ada753eddd85
RealEthanPlayzDev <ethan.budianto@gmail.com>
2021-10-20 12:32:53 +0700

    kitsune command

---
+++ src/commands/fun/kitsune.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+const MeteoriumCommand = require("../../util/Command");
+const Neko = require("neko-love.js");
+
+module.exports = MeteoriumCommand("kitsune", "Random kitsune pictures", async (interaction, client) => {
+    interaction.deferReply();
+    try {
+        const kitsunepic = await Neko("kitsune");
+        await interaction.editReply({ embeds: [
+            new MessageEmbed()
+                .setTitle("Random kitsune picture")
+                .setImage(kitsunepic)
+                .setColor("FF0000")
+        ]});
+    } catch(err) {
+        await interaction.editReply({ embeds: [
+            new MessageEmbed()
+                .setTitle("Failed getting a random kitsunr picture")
+                .setDescription(String(err))
+                .setColor("FF0000")
+                .setFooter("Meteorium | Developed by RadiatedExodus (RealEthanPlayzDev)")
+                .setTimestamp()
+        ]});
+    }
+});
