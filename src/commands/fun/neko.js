76424f3b10ce3588c16fcaa525f521b653a3992f
RealEthanPlayzDev <ethan.budianto@gmail.com>
2021-10-20 11:54:58 +0700

    Fix neko command

--- src/commands/fun/neko.js
+++ src/commands/fun/neko.js
@@ -5,11 +5,11 @@ const Neko = require("neko-love.js");
 module.exports = new MeteoriumCommand("neko", "Random nekomimi/catgirl picture", async (interaction, client) => {
     await interaction.deferReply();
     try {
-        const nekoembed = await Neko("neko");
+        const nekopic = await Neko("neko");
         await interaction.editReply({ embeds: [
             new MessageEmbed()
                 .setTitle("Random neko picture")
-                .setImage(nekoembed.embed.image.ur)
+                .setImage(nekopic)
                 .setColor("0099ff")
         ]});
     } catch(err) {
