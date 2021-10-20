b1918a1b880abcb271a34e176de08324500d65ce
RealEthanPlayzDev <ethan.budianto@gmail.com>
2021-10-20 13:46:38 +0700

    Change type on guild setup event

--- src/events/DBSetupGuildAdded.js
+++ src/events/DBSetupGuildAdded.js
@@ -8,8 +8,8 @@ module.exports = {
             const newGuildSettingSchema = new GuildSettingSchema({
                 GuildId: guild.id,
                 EnforceSayinExecutor: false,
-                DisabledCommands: [],
-                DisabledCommandCategories: [],
+                DisabledCommands: {},
+                DisabledCommandCategories: {},
                 MuteRoleId: ""
             });
             function save() { newGuildSettingSchema.save().then(() => { console.log("Successfully registered schema for guild "+guild.id) }).catch(() => { save() }) }
