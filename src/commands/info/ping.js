b7b35f72b1835dbcceec992c277874169f7685a5
RealEthanPlayzDev <ethan.budianto@gmail.com>
2021-10-27 12:32:35 +0700

    Roundtrip ping addition

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -2,6 +2,6 @@ const { Message } = require("discord.js");
 const MeteoriumCommand = require("../../util/Command");
 
 module.exports = new MeteoriumCommand("ping", "Returns latency number", async (interaction, client) => {
-    //const msg = await interaction.deferReply();
-    interaction.editReply(`Websocket ping: ${client.ws.ping} ms`); // Message ping: ${Date.now() - msg instanceof Message && msg.createdTimestamp || msg.timestamp}\n
+    const msg = await interaction.deferReply({ fetchReply: true });
+    interaction.editReply(`Websocket ping: ${client.ws.ping} ms\nRoundtrip ping: ${msg.createdTimestamp - interaction.createdTimestamp} ms\nRoundtrip ping (local time method): ${msg.createdTimestamp - Date.now()} ms`);
 });
