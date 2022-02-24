10174cb4666bd9dccd615e34973c763a0a24346d
RealEthanPlayzDev <radiatedexodus@gmail.com>
2022-02-24 12:33:03 +0700

    sayin - add reply functionality

--- src/commands/moderation/sayin.js
+++ src/commands/moderation/sayin.js
@@ -9,6 +9,7 @@ module.exports = new MeteoriumCommand("sayin", "Says message in a optional chann
         const showExecutorName = (guildSchema.EnforceSayinExecutor && !interaction.member.permissions.has("ADMINISTRATOR", true) && true || (interaction.options.getBoolean("showexecutorname") === null && true || false))
         const doNotShowSuccessMessage = !(interaction.options.getBoolean("showsuccessmessage") === null && true || interaction.options.getBoolean("showsuccessmessage"));
         const msg = (showExecutorName && interaction.options.getString("message")+`\n\n(Sayin command executed by ${interaction.member})` || interaction.options.getString("message")), channel = interaction.options.getChannel("channel") ? interaction.options.getChannel("channel") : interaction.channel;
+        const replyTo = interaction.options.getString("replyto");
         if (doNotShowSuccessMessage) {
             if (!interaction.member.permissions.has("ADMINISTRATOR", true)) {
                 await interaction.reply({ embeds: [
@@ -33,7 +34,7 @@ module.exports = new MeteoriumCommand("sayin", "Says message in a optional chann
             ], ephemeral: doNotShowSuccessMessage })
             return;
         }
-        await channel.send(msg);
+        await channel.send({content: msg, reply: { messageReference: replyTo }});
         await interaction.reply({ content: "Successfully sent message.", ephemeral: doNotShowSuccessMessage });
     } else {
         await interaction.reply({embeds: [
@@ -51,5 +52,6 @@ module.exports = new MeteoriumCommand("sayin", "Says message in a optional chann
     .addStringOption(option => option.setName("message").setDescription("The message to be sent").setRequired(true))
     .addChannelOption(option => option.setName("channel").setDescription("Optional channel where message will be sent (if not specified it will be sent to the current channel"))
     .addBooleanOption(option => option.setName("showexecutorname").setDescription("Show the executor name or not (can be overriden by EnforceSayinExecutor, doesn't include admins)"))
-    .addBooleanOption(option => option.setName("showsuccessmessage").setDescription("Shows the success message or not (if false then success message only shows to you)").setRequired(false))
+    .addBooleanOption(option => option.setName("showsuccessmessage").setDescription("Shows the success message or not (if false then success message only shows to you)"))
+    .addStringOption(option => option.setName("replyto").setDescription("Fill with the message id of the target message you want to reply to"))
 );
