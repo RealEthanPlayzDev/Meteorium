1589928406acdb6f188cc93fbb456616d1b12e36
RealEthanPlayzDev <radiatedexodus@gmail.com>
2022-02-26 13:36:57 +0700

    Remove 2 obsolete util classes

--- src/util/SCRAPPED_MusicPlayer.js
+++
@@ -1,9 +0,0 @@
-// !! THIS WAS SCRAPPED !!
-// Class for handling the music system for Meteorium
-// Written by RadiatedExodus (ItzEthanPlayz_YT/RealEthanPlayzDev)
-
-class MeteoriumMusicPlayer {
-    constructor() {
-        this.playerqueue = new Map();
-    }
-}
