a3d0aea41b0ab76def26f35776fdbc485cc9f31f
RealEthanPlayzDev <radiatedexodus@gmail.com>
2022-02-26 13:19:15 +0700

    CommandHandler: Migrate to MeteoriumEmbed help: Fix deprecation warning

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -4,13 +4,9 @@ const MeteoriumCommand = require("../../util/Command");
 
 module.exports = new MeteoriumCommand("help", "Shows information about the bot", async (interaction, client) => {
     await interaction.reply({ embeds: [
-        new MeteoriumEmbed("", "A Discord bot developed by RadiatedExodus (ItzEthanPlayz_YT/RealEthanPlayzDev) as a side (and personal) project, written in Javascript using Node.js and Discord.js, also being used as a way for me to learn Javascript.").setAuthor("Meteorium", null, "https://github.com/RealEthanPlayzDev/Meteorium")
+            new MeteoriumEmbed("", "A Discord bot developed by RadiatedExodus (ItzEthanPlayz_YT/RealEthanPlayzDev) as a side (and personal) project, written in Javascript using Node.js and Discord.js, also being used as a way for me to learn Javascript.").setAuthor({name: "Meteorium", url: "https://github.com/RealEthanPlayzDev/Meteorium"})
         ], components: [
-        new MessageActionRow().addComponents(
-            new MessageButton()
-                .setStyle("LINK")
-                .setURL("https://github.com/RealEthanPlayzDev/Meteorium")
-                .setLabel("Open GitHub repository")
-        )
-    ]});
+            new MessageActionRow().addComponents(new MessageButton().setStyle("LINK").setURL("https://github.com/RealEthanPlayzDev/Meteorium").setLabel("Open GitHub repository"))
+        ]
+    });
 });
