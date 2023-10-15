a055535da426a342855904d4d0a3fe4b96b660c5
RealEthanPlayzDev <radiatedexodus@gmail.com>
2023-10-15 22:29:09 +0700

    more intents?

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@ import { MeteoriumClient } from "./util/MeteoriumClient";
 import { GatewayIntentBits } from "discord.js";
 
 const Client = new MeteoriumClient({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildVoiceStates],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildModeration
+    ],
 });
 
 Client.login();
