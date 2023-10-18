695a38ef6d22c7d4597aad32cab603fdb301af6b
RadiatedExodus <radiatedexodus@gmail.com>
2023-10-18 18:18:31 +0700

    Update index.ts

--- src/events/index.ts
+++ src/events/index.ts
@@ -4,6 +4,9 @@ import type { MeteoriumClient } from "../util/MeteoriumClient";
 export * as ready from "./ready";
 export * as interactionCreate from "./interactionCreate";
 export * as guildCreate from "./guildCreate";
+export * as shardResume from "./shardResume";
+export * as guildMemberAdd from "./guildMemberAdd";
+export * as guildMemberRemove from "./guildMemberRemove";
 
 export type MeteoriumEvent<EventName extends keyof ClientEvents> = {
     Callback(client: MeteoriumClient, ...args: ClientEvents[EventName]): Awaitable<any>;
