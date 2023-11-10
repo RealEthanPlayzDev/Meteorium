fab080b609e2065bee7296a2ac92fb3b213bdc23
RealEthanPlayzDev <radiatedexodus@gmail.com>
2023-11-10 14:11:40 +0700

    Unban command

---
+++ src/commands/Moderation/Unban.ts
@@ -0,0 +1,116 @@
+import { ModerationAction } from "@prisma/client";
+import { SlashCommandBuilder } from "discord.js";
+import type { MeteoriumCommand } from "..";
+import { MeteoriumEmbedBuilder } from "../../util/MeteoriumEmbedBuilder";
+
+export const Command: MeteoriumCommand = {
+    InteractionData: new SlashCommandBuilder()
+        .setName("unban")
+        .setDescription("Unban someone that is banned and create a new case regarding it")
+        .addUserOption((option) => option.setName("user").setDescription("The user to be unbanned").setRequired(true))
+        .addStringOption((option) =>
+            option.setName("reason").setDescription("The reason on why the user was unbanned").setRequired(true),
+        )
+        .addAttachmentOption((option) =>
+            option
+                .setName("proof")
+                .setDescription("An media containing proof to prove the reason valid")
+                .setRequired(false),
+        ),
+    async Callback(interaction, client) {
+        if (!interaction.member.permissions.has("BanMembers"))
+            return await interaction.editReply({
+                content: "You do not have permission to unban users from this server.",
+            });
+
+        const User = interaction.options.getUser("user", true);
+        const Reason = interaction.options.getString("reason", true);
+        const AttachmentProof = interaction.options.getAttachment("proof", false);
+        const GuildSchema = (await client.Database.guild.findUnique({ where: { GuildId: interaction.guildId } }))!;
+
+        if (User.id == interaction.user.id)
+            return await interaction.reply({ content: "the what", ephemeral: true });
+        if (User.bot)
+            return await interaction.reply({ content: "You can't ban bots so unban the bot manually", ephemeral: true });
+
+        await client.Database.guild.update({
+            where: { GuildId: interaction.guildId },
+            data: { CurrentCaseId: GuildSchema.CurrentCaseId + 1 },
+        });
+        const CaseResult = await client.Database.moderationCase.create({
+            data: {
+                CaseId: GuildSchema.CurrentCaseId + 1,
+                Action: ModerationAction.Unban,
+                TargetUserId: User.id,
+                ModeratorUserId: interaction.user.id,
+                GuildId: interaction.guildId,
+                Reason: Reason,
+                AttachmentProof: AttachmentProof ? AttachmentProof.url : "",
+            },
+        });
+        await interaction.guild.members.unban(User, `Case ${CaseResult.CaseId} by ${interaction.user.username} (${interaction.user.id}): ${Reason}`);
+
+        const LogEmbed = new MeteoriumEmbedBuilder(undefined, interaction.user)
+            .setAuthor({
+                name: `Case: #${CaseResult.CaseId} | unban | ${User.username}`,
+                iconURL: User.displayAvatarURL({ extension: "png" }),
+            })
+            .addFields(
+                { name: "User", value: `<@${User.id}>` },
+                {
+                    name: "Moderator",
+                    value: `<@${interaction.user.id}>`,
+                },
+                { name: "Reason", value: Reason },
+            )
+            .setImage(AttachmentProof ? AttachmentProof.url : null)
+            .setFooter({ text: `Id: ${User.id}` })
+            .setTimestamp()
+            .setColor("Red");
+
+        const PublicModLogChannel = await interaction.guild.channels
+            .fetch(GuildSchema.PublicModLogChannelId)
+            .catch(() => null);
+        if (PublicModLogChannel && PublicModLogChannel.isTextBased())
+            await PublicModLogChannel.send({ embeds: [LogEmbed] });
+
+        const GuildSetting = await client.Database.guild.findUnique({ where: { GuildId: interaction.guild.id } });
+        if (GuildSetting && GuildSetting.LoggingChannelId != "")
+            client.channels
+                .fetch(GuildSetting.LoggingChannelId)
+                .then(async (channel) => {
+                    if (channel && channel.isTextBased())
+                        await channel.send({
+                            embeds: [
+                                new MeteoriumEmbedBuilder(undefined, interaction.user)
+                                    .setTitle("Moderation action")
+                                    .setFields([
+                                        { name: "Case id", value: String(CaseResult.CaseId) },
+                                        {
+                                            name: "Moderator",
+                                            value: `${interaction.user.username} (${interaction.user.id}) (<@${interaction.user.id}>)`,
+                                        },
+                                        {
+                                            name: "Offending user",
+                                            value: `${User.username} (${User.id}) (<@${User.id}>)`,
+                                        },
+                                        { name: "Action", value: "Unban" },
+                                        { name: "Reason", value: Reason },
+                                        { name: "Proof", value: AttachmentProof ? AttachmentProof.url : "N/A" },
+                                    ])
+                                    .setImage(AttachmentProof ? AttachmentProof.url : null),
+                            ],
+                        });
+                })
+                .catch(() => null);
+
+        return await interaction.reply({
+            content:
+                PublicModLogChannel != null && PublicModLogChannel.isTextBased()
+                    ? undefined
+                    : "(Warning: could not send log message to the public mod log channel)",
+            embeds: [LogEmbed],
+            ephemeral: GuildSchema?.PublicModLogChannelId != "",
+        });
+    },
+};
