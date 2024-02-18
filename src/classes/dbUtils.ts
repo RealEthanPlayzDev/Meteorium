d162bd0848fb34235db69c3ab719614d12cf2e21
RealEthanPlayzDev <radiatedexodus@gmail.com>
2024-02-18 16:25:31 +0700

    DatabaseUtils: generateCaseEmbedFromData & generateCaseEmbedFromCaseId

--- src/classes/dbUtils.ts
+++ src/classes/dbUtils.ts
@@ -1,5 +1,7 @@
-import MeteoriumClient from "./client.js";
+import { User, time, userMention } from "discord.js";
 import { ModerationAction } from "@prisma/client";
+import MeteoriumClient from "./client.js";
+import MeteoriumEmbedBuilder from "./embedBuilder.js";
 
 export type CaseData = {
     GlobalCaseId: number;
@@ -90,5 +92,85 @@ export default class MeteoriumDatabaseUtilities {
         }
 
         return latestCases;
+    }
+
+    public async generateCaseEmbedFromData(
+        caseData: CaseData,
+        requester?: User,
+        full?: boolean,
+        inclRequester?: boolean,
+    ): Promise<MeteoriumEmbedBuilder> {
+        // Get user datas
+        const modUser = await this.client.users.fetch(caseData.ModeratorUserId).catch(() => null);
+        const targetUser = await this.client.users.fetch(caseData.TargetUserId).catch(() => null);
+
+        const embed = new MeteoriumEmbedBuilder(requester);
+
+        // Set the author field
+        embed.setAuthor({
+            name: `Case #${caseData.CaseId} | ${caseData.Action.toLowerCase()} | ${targetUser != null ? `${targetUser.username} (${targetUser.id})` : caseData.TargetUserId}`,
+            iconURL: targetUser != null ? targetUser.displayAvatarURL({ extension: "png", size: 256 }) : undefined,
+        });
+
+        // Set the attachment proof if any exist
+        embed.setThumbnail(caseData.AttachmentProof == "" ? null : caseData.AttachmentProof);
+
+        // Include requester?
+        if (inclRequester && requester)
+            embed.addFields([
+                { name: "Requester", value: `${userMention(requester.id)} (${requester.username} - ${requester.id})` },
+            ]);
+
+        // Add target and moderator fields
+        embed.addFields([
+            {
+                name: "Moderator",
+                value: `${userMention(caseData.ModeratorUserId)} (${modUser != null ? `${modUser.username} - ${modUser.id}` : caseData.ModeratorUserId})`,
+            },
+            {
+                name: "Target",
+                value: `${userMention(caseData.TargetUserId)} (${targetUser != null ? `${targetUser.username} - ${targetUser.id}` : caseData.TargetUserId})`,
+            },
+        ]);
+
+        // Reason
+        embed.addFields([{ name: "Reason", value: caseData.Reason }]);
+
+        // Duration
+        if (caseData.Action == ModerationAction.Mute || caseData.Action == ModerationAction.TempBan)
+            embed.addFields([{ name: "Duration", value: caseData.Duration }]);
+
+        // Appealable
+        if (caseData.Action == ModerationAction.Ban)
+            embed.addFields([{ name: "Appealable", value: caseData.NotAppealable ? "No" : "Yes" }]);
+
+        // Full data
+        if (full) {
+            embed.addFields([
+                { name: "Moderator note", value: caseData.ModeratorNote == "" ? "N/A" : caseData.ModeratorNote },
+                { name: "Removed", value: caseData.Removed ? "Yes" : "No" },
+            ]);
+            embed.setImage(caseData.ModeratorAttachment == "" ? null : caseData.ModeratorAttachment);
+        }
+
+        // Created at
+        embed.addFields([
+            { name: "Created at", value: `${time(caseData.CreatedAt, "F")} (${time(caseData.CreatedAt, "R")})` },
+        ]);
+
+        return embed;
+    }
+
+    public async generateCaseEmbedFromCaseId(
+        guildId: string,
+        caseId: number,
+        requester?: User,
+        full?: boolean,
+        historyTake?: number,
+        inclRequester?: boolean,
+    ): Promise<MeteoriumEmbedBuilder | false> {
+        const caseData = await this.getCaseData(guildId, caseId, historyTake);
+        if (!caseData) return false;
+        return await this.generateCaseEmbedFromData(caseData, requester, full, inclRequester);
     }
 }
